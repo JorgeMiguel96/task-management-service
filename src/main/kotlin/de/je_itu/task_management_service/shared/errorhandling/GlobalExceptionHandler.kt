78e6ff9b90eba441dcff833eebfa20e278902754
TutaevaIrina <s0582520@htw-berlin.de>
2025-04-11 10:53:28 +0200

    BE-0002: Create an Exception for Taskname to avoid duplication and design also a GlobalExceptionHandler

---
+++ src/main/kotlin/de/je_itu/task_management_service/shared/errorhandling/GlobalExceptionHandler.kt
@@ -0,0 +1,39 @@
+package de.je_itu.task_management_service.shared.errorhandling
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @ExceptionHandler(
+        TaskNameIsAlreadyExistException::class
+    )
+    fun handleConflictExceptions(ex: BusinessException) =
+        respondWith(CONFLICT, ex.message).also { ex.message }
+
+    @ExceptionHandler(
+        HttpMessageNotReadableException::class
+    )
+    fun handleHttpMessageNotReadableException(ex: HttpMessageNotReadableException) =
+        respondWith(BAD_REQUEST, ex.cause?.cause?.message).also { ex.message }
+
+
+    @ExceptionHandler(
+        Exception::class
+    )
+    fun handleGenericException(ex: Exception) =
+        respondWith(INTERNAL_SERVER_ERROR, "An internal error occurred.")
+            .also { logger.error(ex.message, ex.javaClass.simpleName) }
+
+
+    private fun respondWith(responseStatus: HttpStatus, message: String?) =
+        ResponseEntity.status(responseStatus).body(message)
+}
--- src/main/kotlin/de/je_itu/task_management_service/task/persistence/TaskRepository.kt
+++ src/main/kotlin/de/je_itu/task_management_service/task/persistence/TaskRepository.kt
@@ -1,7 +1,10 @@
 package de.je_itu.task_management_service.task.persistence
 
+import de.je_itu.task_management_service.shared.types.Taskname
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TaskRepository: CrudRepository<TaskEntity, Long>
+interface TaskRepository: CrudRepository<TaskEntity, Long> {
+    fun existsByTaskName(taskName: Taskname): Boolean
+}
